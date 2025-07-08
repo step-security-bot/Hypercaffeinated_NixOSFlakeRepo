267e3ed3a8da51caf7748b31d9aafba98d44631f
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-07-09 01:57:19 +0200

    Update chaotic.nix

--- modules/chaotic.nix
+++ modules/chaotic.nix
@@ -3,7 +3,23 @@
 {
 
   chaotic = {
-    
+    nyx = {
+      cache = {
+        enable = true;
+      };
+      nixPath = {
+        enable = true;
+      };
+      overlay = {
+        enable = true;
+      };
+      registry = {
+        enable = true;
+      };
+    };
+    appmenu-gtk3-module = {
+      enable = true;
+    };
   };
   
 }
