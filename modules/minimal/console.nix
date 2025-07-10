a0da692bc9fe1250aa13c5a879d059fd13817552
Hypercaffeinated <34581596+Hypercaffeinated@users.noreply.github.com>
2025-07-10 22:41:57 +0200

    Update and rename modules/console.nix to modules/minimal/console.nix

--- modules/minimal/console.nix
+++ modules/minimal/console.nix
@@ -3,9 +3,8 @@
 {
 
   console = {
-    font = "sun12x22"; # latarcyrheb-sun32 # Lat2-Terminus16
+    font = "latarcyrheb-sun32"; # sun12x22 # Lat2-Terminus16
     useXkbConfig = true;
-    #keyMap = "de";
     earlySetup = true;
   };
 
